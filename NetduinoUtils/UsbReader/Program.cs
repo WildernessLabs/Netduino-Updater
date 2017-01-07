fe49e5d97e052b38a59ab05875541ddf6c00009c
Brian Kim <bkim237@gmail.com>
2017-01-06 17:33:56 -0800

    filter vendors

--- NetduinoUtils/UsbReader/Program.cs
+++ NetduinoUtils/UsbReader/Program.cs
@@ -3,6 +3,7 @@ using LibUsbDotNet;
 using LibUsbDotNet.Info;
 using LibUsbDotNet.Main;
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
 
 namespace UsbReader
 {
@@ -12,36 +13,41 @@ namespace UsbReader
 
 		public static void Main(string[] args)
 		{
+			List<short> validVendorIDs = new List<short>
+			{
+				0x22B1, // secret labs
+				0x1B9F, // ghi
+				0x05A // who knows
+			};
+
 			// Dump all devices and descriptor information to console output.
 			UsbRegDeviceList allDevices = UsbDevice.AllDevices;
 			foreach (UsbRegistry usbRegistry in allDevices)
 			{
 				if (usbRegistry.Open(out MyUsbDevice))
 				{
+					if (!validVendorIDs.Contains(MyUsbDevice.Info.Descriptor.VendorID)){ continue; } 
+
 					Console.WriteLine(MyUsbDevice.Info.ToString());
-					for (int iConfig = 0; iConfig < MyUsbDevice.Configs.Count; iConfig++)
-					{
-						UsbConfigInfo configInfo = MyUsbDevice.Configs[iConfig];
-						//Console.WriteLine(configInfo.ToString());
 
-						ReadOnlyCollection<UsbInterfaceInfo> interfaceList = configInfo.InterfaceInfoList;
-						for (int iInterface = 0; iInterface < interfaceList.Count; iInterface++)
-						{
-							UsbInterfaceInfo interfaceInfo = interfaceList[iInterface];
-							//Console.WriteLine(interfaceInfo.ToString());
+					//for (int iConfig = 0; iConfig < MyUsbDevice.Configs.Count; iConfig++)
+					//{
+					//	UsbConfigInfo configInfo = MyUsbDevice.Configs[iConfig];
+					//	//Console.WriteLine(configInfo.ToString());
 
-							ReadOnlyCollection<UsbEndpointInfo> endpointList = interfaceInfo.EndpointInfoList;
-							for (int iEndpoint = 0; iEndpoint < endpointList.Count; iEndpoint++)
-							{
-								var endpoint = endpointList[iEndpoint];
-								if (endpoint.Descriptor.DescriptorType == LibUsbDotNet.Descriptors.DescriptorType.Device)
-								{
-									Console.WriteLine(endpointList[iEndpoint].ToString());
-								}
+					//	ReadOnlyCollection<UsbInterfaceInfo> interfaceList = configInfo.InterfaceInfoList;
+					//	for (int iInterface = 0; iInterface < interfaceList.Count; iInterface++)
+					//	{
+					//		UsbInterfaceInfo interfaceInfo = interfaceList[iInterface];
+					//		//Console.WriteLine(interfaceInfo.ToString());
 
-							}
-						}
-					}
+					//		ReadOnlyCollection<UsbEndpointInfo> endpointList = interfaceInfo.EndpointInfoList;
+					//		for (int iEndpoint = 0; iEndpoint < endpointList.Count; iEndpoint++)
+					//		{
+					//			Console.WriteLine(endpointList[iEndpoint].ToString());
+					//		}
+					//	}
+					//}
 				}
 			}
 
