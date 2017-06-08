dd24f4243e505fc5c69aed0b4c658b76a8640375
Brian Kim <bkim237@gmail.com>
2017-06-08 13:29:20 -0700

    add remaining nd types

--- NetduinoUtils/NetduinoDeploy/Globals.cs
+++ NetduinoUtils/NetduinoDeploy/Globals.cs
@@ -6,13 +6,14 @@ namespace NetduinoDeploy
     public static class Globals
     {
         static Globals()
-        {
-            DeviceTypes = new List<Device>();
-            //DeviceTypes.Add(new Device() { ProductID = 5, Name = "Netduino 2" });
+		{
+			DeviceTypes = new List<Device>();
+			DeviceTypes.Add(new Device() { ProductID = 4, Name = "Netduino Go" });
             DeviceTypes.Add(new Device() { ProductID = 5, Name = "Netduino Plus 2" });
+            DeviceTypes.Add(new Device() { ProductID = 6, Name = "Netduino 2" });
             DeviceTypes.Add(new Device() { ProductID = 7, Name = "Netduino 3" });
             DeviceTypes.Add(new Device() { ProductID = 8, Name = "Netduino 3 Ethernet" });
-            //DeviceTypes.Add(new Device() { ProductID = 5, Name = "Netduino 3 Wifi" });
+            DeviceTypes.Add(new Device() { ProductID = 9, Name = "Netduino 3 Wifi" });
         }
 
         public static List<Device> DeviceTypes { get; set; }
