9eca27413ad177918d87c136505e6187b1289bf7
Adrian Stevens <adrian.stevens@gmail.com>
2018-03-13 09:35:11 -0700

    NetduinoDeploy X.F. - added ShowDialg macOS code

--- NetduinoUtils/NetduinoDeployXF/NetduinoDeploy/NetduinoDeploy.macOS/OpenFileDialog.cs
+++ NetduinoUtils/NetduinoDeployXF/NetduinoDeploy/NetduinoDeploy.macOS/OpenFileDialog.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.Linq;
+using System.Collections.Generic;
+using AppKit;
+
 namespace NetduinoDeploy.macOS
 {
     public class OpenFileDialog : IOpenFileDialog
@@ -11,7 +14,44 @@ namespace NetduinoDeploy.macOS
 
         public bool ShowDialog(string filter)
         {
-            return false;
+            var extensions = GetFileExtensionsFromFilter(filter).ToArray();
+
+            var dlg = NSOpenPanel.OpenPanel;
+            dlg.CanChooseFiles = true;
+            dlg.CanChooseDirectories = false;
+            dlg.AllowedFileTypes = extensions;
+            dlg.AllowsMultipleSelection = true;
+
+            if(dlg.RunModal() == 1)
+            {
+                FileNames = dlg.Urls.Select(u => u.Path).ToArray();
+            }
+            else
+            {
+                FileNames = new string[0];
+            }
+
+            return (FileNames.Length > 0) ? true : false;
+        }
+
+        List<string> GetFileExtensionsFromFilter(string filter)
+        {
+            var results = new List<string>();
+
+            int index = 0;
+
+            while(true)
+            {
+                index = filter.IndexOf("|*.", index);
+
+                if (index == -1)
+                    break;
+
+                var extension = filter.Substring(index += 3, 3);
+                results.Add(extension);
+            }
+
+            return results;
         }
     }
 }
